d1927f2fa192465c3193800efd0fd90f82666cff
Petrushchenko <petrushchenko.l@ukr.net>
2019-08-06 07:58:26 +0300

    Updates

---
+++ precache-manifest.c08e1fb9fb92aca9d76c7f7c00e01b51.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1480408a9eb3c4b595ca958cb3bb993",
+    "url": "/postss/index.html"
+  },
+  {
+    "revision": "523aa87a52044c94af1e",
+    "url": "/postss/static/css/main.8af51374.chunk.css"
+  },
+  {
+    "revision": "2974be76d51bf2335a06",
+    "url": "/postss/static/js/2.2c15429c.chunk.js"
+  },
+  {
+    "revision": "523aa87a52044c94af1e",
+    "url": "/postss/static/js/main.37501140.chunk.js"
+  },
+  {
+    "revision": "4ee77c3225aa69f9b327",
+    "url": "/postss/static/js/runtime~main.7031d743.js"
+  },
+  {
+    "revision": "96e6a05d24fe23ec5e079272605c5070",
+    "url": "/postss/static/media/checked-green.96e6a05d.svg"
+  },
+  {
+    "revision": "161834d61a0a1e0139f304215b34d207",
+    "url": "/postss/static/media/checked.161834d6.svg"
+  },
+  {
+    "revision": "91f81a037c6b74da0a06841a846d6418",
+    "url": "/postss/static/media/circle.91f81a03.svg"
+  },
+  {
+    "revision": "166e2800abb59ea41cc1eab511ca0fd0",
+    "url": "/postss/static/media/star-colored.166e2800.svg"
+  },
+  {
+    "revision": "88688cf0265e297262d0cf7470c1609c",
+    "url": "/postss/static/media/star.88688cf0.svg"
+  }
+]);
